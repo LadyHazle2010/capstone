083cadcfaf4ce4f5525b840f0bd5a4f5a6fde459
Ladyhazle2010 <sweetladyhazle@gmail.com>
2023-07-12 22:04:06 -0500

    frontend/store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import Layout from "./hocs/Layout";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./hocs/Routes";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes />
-      </Layout>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Layout>
+          <Routes />
+        </Layout>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
